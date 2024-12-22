db4e1836031c5a191416007793befa040c2145b3
Muxitdin <toraananas@gmail.com>
2024-12-22 12:53:42 +0500

    added search feature / some refactoring

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -2,7 +2,6 @@ const Message = require('../models/message');
 const Conversation = require('../models/conversation');
 const { io, checkUserIsActive } = require('../config/socket');
 
-
 const sendMessage = async (req, res) => {
     try {
         // todo: Xabar jo'natish uchun mo'ljallangan controller funksiyada quyidagicha ish bajariladi:
@@ -35,9 +34,10 @@ const sendMessage = async (req, res) => {
 
         // todo: Barcha o'zgarishlar Database'ga saqlanadi
         await Promise.all([conversation.save(), newMessage.save()]);
+        await newMessage.populate('sender receiver')
 
         const activeUserSocketId = checkUserIsActive(receiver);
-        if (activeUserSocketId) io.to(activeUserSocketId).emit("getNewMessage", newMessage);
+        if (activeUserSocketId) io.to(activeUserSocketId).emit("getNewMessage", { newMessage, sender })
 
         // todo: Eng so'nggida chaqiruvchiga yangi xabar qaytarib beriladi
         res.status(201).json(newMessage);
@@ -57,14 +57,19 @@ const getMessages = async (req, res) => {
         // todo: Izlab topilgan xabarlarni populate qilish
         const conversation = await Conversation.findOne({
             participants: { $all: [sender, receiver] }
-        }).populate([{
+        }).populate({
             path: "messages",
-            model: "Message",
-            populate: [
-                { path: "sender", model: "Auth" },
-                { path: "receiver", model: "Auth" }
-            ]
-        }]);
+            populate: ["sender", "receiver"]
+        });
+        // .populate([{
+        //     path: "messages",
+        //     model: "Message",
+        //     populate: [
+        //         { path: "sender", model: "Auth" },
+        //         { path: "receiver", model: "Auth" }
+        //     ]
+        // }]);
+        
         if (!conversation) return res.status(200).send([]);
 
         // todo: Yakunda chaqiruvchiga topilgan xabarlarni qaytarish
